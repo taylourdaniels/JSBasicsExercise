fd5a30d8b376d02bed7a0408533c6c6ebcd7dfbe
taylourdaniels <taylour.daniels@outlook.com>
2021-09-21 08:01:24 -0500

    Basic Javascript Mechanics

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(14)
+
+/*
+String
+Number
+Boolean
+Undefined
+Null
+*/
+
+/*
+
++
+-
+*
+/
+**
+
+*/
+
+console.log(4%3)
+console.log(4/3)
+console.log('I\'M MARY POPPINS Y\'ALL')
+
+console.log('Hawk' + 'eye')
+
+var firstName = 'Taylour'
+var lastName = 'Daniels'
+console.log(firstName.length + lastName.length)
+
+var endgameAvengers = 'Avengers: Endgame'
+console.log(endgameAvengers.charAt(endgameAvengers.length-1))
